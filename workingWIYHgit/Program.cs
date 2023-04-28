87c744e9b42bc4f22c6f366f6057c214b32ab747
Арсен Гиоев <gioev.arsen15@list.ru>
2023-04-28 09:41:57 +0300

    Исправлен вывод текста

--- workingWIYHgit/Program.cs
+++ workingWIYHgit/Program.cs
@@ -10,7 +10,7 @@ namespace workingWIYHgit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello World!");
         }
     }
 }
